47abb266d3f1eb59a18fdb739734902f11dc40fb
Theis Pieter Hollebeek <tphollebeek@gmail.com>
2024-11-15 21:48:01 +0100

    pwogwess

--- events.ts
+++ events.ts
@@ -2,17 +2,16 @@ export type Dated = { date: Date };
 
 export type Restock = Dated & {
   type: "restock";
-  amount: number;
+  pillsLeft: number;
 };
 
 export type DoseChanged = Dated & {
   type: "dose_changed";
-  per_day: number;
+  dosePerDay: number;
 };
 
 export type EndOfDay = Dated & {
   type: "day_end";
-  per_day: number;
 };
 
 export type Created =
@@ -42,7 +41,82 @@ type State = {
   pillsLeft: number;
 };
 
-export function collapseEvents(
+export function doStuff(created: Created, events: Event[]): Day[] {
+  const prepared = insertEndOfDayEvents(created, events);
+  const state: State = {
+    today: new Date(created.date),
+    dosePerDay: created.dosePerDay,
+    pillsLeft: created.pillsLeft,
+  };
+  const collapsed = evaluateEvents(state, events, []);
+  return collapsed;
+}
+
+function dateToNumber(date: Date): number {
+  /// 18th may, 2004 -> 20040518
+  return 10000 * date.getFullYear() + 100 * (date.getMonth() + 1) +
+    date.getDate();
+}
+
+function dateToString(date: Date): string {
+  /// 18th may, 2004 -> "20040518"
+  return dateToNumber(date).toString();
+}
+
+function uniqueDatesFromEvents(created: Created, events: Event[]): number[] {
+  const createdDate = dateToNumber(created.date);
+  const dates = [createdDate];
+  for (const event of events) {
+    const date = dateToNumber(event.date);
+    if (date < createdDate) {
+      throw new Error("panic: created was not the first event found");
+    }
+    if (dates.includes(date)) {
+      continue;
+    }
+    dates.push(dateToNumber(event.date));
+  }
+  dates.sort();
+  return dates;
+}
+
+function insertEndOfDayEvents(created: Created, events: Event[]): Event[] {
+  const dates: { [key: string]: Event[] } = {};
+  const modifiedEvents = structuredClone(events);
+  const counter = new Date(created.date);
+  while (true) {
+    const date = dateToString(counter);
+    if (!dates[date]) {
+      const endOfDayDate = structuredClone(counter);
+      endOfDayDate.setHours(23);
+      endOfDayDate.setMinutes(59);
+      endOfDayDate.setSeconds(59);
+      const endOfDay: EndOfDay = {
+        type: "day_end",
+        date: endOfDayDate,
+      };
+      dates[date] = [endOfDay];
+    }
+    const event = modifiedEvents.shift();
+    if (!event) {
+      break;
+    }
+    if (date !== dateToString(event.date)) {
+      modifiedEvents.unshift(event);
+      counter.setDate(counter.getDate() + 1);
+      continue;
+    }
+    dates[date].push(event);
+    counter.setDate(counter.getDate() + 1);
+  }
+
+  return Object
+    .values(dates)
+    .reduce((acc, v) => [...acc, ...v])
+    .toSorted((a, b) => a.date.getTime() - b.date.getTime());
+}
+
+function evaluateEvents(
   state: State,
   events: Event[],
   days: Day[],
@@ -52,20 +126,20 @@ export function collapseEvents(
     return days;
   }
   if (!sameDay(state.today, current.date)) {
-    throw new Error("panic: skipped an evaluate");
+    throw new Error("panic: skipped a day");
   }
   switch (current.type) {
     case "dose_changed":
-      state.dosePerDay = current.per_day;
+      state.dosePerDay = current.dosePerDay;
       break;
     case "day_end":
       state.pillsLeft -= state.dosePerDay;
       state.today.setDate(state.today.getDate() + 1);
       break;
     case "restock": {
-      state.pillsLeft += current.amount;
+      state.pillsLeft += current.pillsLeft;
       break;
     }
   }
-  return collapseEvents(state, events, days);
+  return evaluateEvents(state, events, days);
 }
